d8a7f058c20260fab9a0754858d179ff3fd8a546
Micheli <michelimelki@gmail.com>
2025-08-29 15:35:22 -0300

    Adicionando navbar e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './pages/components/navbar/Navbar'
 import Home from './pages/home/Home'
+import Footer from './pages/components/footer/Footer'
 
 function App() {
     return (
-        <Home />
+      <BrowserRouter>
+        <Navbar/>
+        <div className="min-h-[80vh">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                               
+
+            </Routes>
+        </div>
+        <Footer/>
+      </BrowserRouter>
+      
+
+     
     )
 }
 
