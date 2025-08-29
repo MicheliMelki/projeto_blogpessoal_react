464a3ecf9b416f1eb4186d71b51e90b437cff3d1
Micheli <michelimelki@gmail.com>
2025-08-29 16:37:08 -0300

    Adicionandos telas de Login e Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './pages/components/navbar/Navbar'
-import Home from './pages/home/Home'
-import Footer from './pages/components/footer/Footer'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Navbar from "./pages/components/navbar/Navbar";
+import Footer from "./pages/components/footer/Footer";
 
 function App() {
-    return (
+  return (
+    <>
       <BrowserRouter>
-        <Navbar/>
-        <div className="min-h-[80vh">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/home" element={<Home />} />
-                               
-
-            </Routes>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
         </div>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
-      
-
-     
-    )
+    </>
+  );
 }
 
-export default App
+export default App;
